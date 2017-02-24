3cae8de8561c36abfca36e219a46872dcbef9ceb
Billy Melago <billymelago@gmail.com>
2017-02-23 22:00:39 -0500

    SWAPI API Project
    back at it

---
+++ js/app.js
@@ -0,0 +1,148 @@
+var $, $searchField, $submitButton, peoppleAPI, person, swapiOptions, nameHTML, filmAPI, film, swapiOptionsF, filmHTML, keyword, keywordAPI, swapiOptionsK, keywordHTML, films, filmsHTML;
+$(document).ready(function () {
+    "use strict";
+    $('.modal').hide();
+    $('form').submit(function (evt) {
+        evt.preventDefault();
+        $searchField = $('#search');
+        $submitButton = $('#submit');
+        $searchField.prop('disabled', true);
+        $submitButton.attr('disabled', true).val('Looking...');
+
+        // the AJAX part
+        peoppleAPI = "http://swapi.co/api/people/";
+        person = $('#search').val().toLowerCase();
+        swapiOptions = {
+            search: person,
+            format: "json"
+        };
+        function displayName(data) {
+            nameHTML = '<ul>';
+            $.each(data.results, function (i, name) {
+                nameHTML += '<li class="names">';
+                nameHTML += '<h2>' + name.name + '</h2>';
+                nameHTML += '</li>';
+            }); // end each
+            
+            nameHTML += '</ul>';
+            $('#photos').html(nameHTML);
+            $searchField.prop('disabled', false);
+            $submitButton.attr('disabled', false).val('Search');
+        }
+        $.getJSON(peoppleAPI, swapiOptions, displayName);
+
+        // the AJAX films part
+        filmAPI = "http://swapi.co/api/films/";
+        film = $('#search').val().toLowerCase();
+        swapiOptionsF = {
+            search: film,
+            format: "json"
+        };
+        function displayFilm(data) {
+            filmHTML = '<ul>';
+            $.each(data.results, function (i, film) {
+                filmHTML += '<li class="films">';
+                filmHTML += '<h2>' + film.title + '</h2>';
+                filmHTML += '</li>';
+            }); // end each
+            filmHTML += '</ul>';
+            $('#photos').html(filmHTML);
+            $searchField.prop('disabled', false);
+            $submitButton.attr('disabled', false).val('Search');
+        }
+        $.getJSON(filmAPI, swapiOptionsF, displayFilm);
+
+        // the AJAX keyword part
+        keyword = $('#search').val().toLowerCase();
+        keywordAPI = "http://swapi.co/api/" + keyword + '/';
+        swapiOptionsK = {
+            //search: keyword,
+            format: "json"
+        };
+        function displayKeyword(data) {
+            keywordHTML = '<div>';
+            keywordHTML += '<span class="keyword">';
+            keywordHTML += '<h2>In the Star Wars API there are a total of  ' + data.count + ' ' + keyword + '!</h2>';
+            keywordHTML += '</span>';
+            keywordHTML += '</div>';
+            $('#photos').html(keywordHTML);
+            $searchField.prop('disabled', false);
+            $submitButton.attr('disabled', false).val('Search');
+        }
+        $.getJSON(keywordAPI, swapiOptionsK, displayKeyword);    
+    }); // end click
+    
+    //Navigation click loads first page of swapi results
+    $('.resource').click(function () {
+        $(".resource").removeClass("selected");
+        $(this).addClass("selected");
+        var resource = $(this).text().toLowerCase(),
+            page = 1,
+            swapiAPI = "http://swapi.co/api/" + resource + "/?page=" + page,
+            swapiOptionsR = {
+            };
+        var nameHTML = '<p>' + resource.toUpperCase() + ' Page: ' + page + '</p>';
+        function displayResource(data) {
+            nameHTML += '<ul>';
+            $.each(data.results, function (i, name) {
+                nameHTML += '<li class="names">';
+                nameHTML += '<h2>' + name.name  + '</h2>';
+                nameHTML += '</li>';
+            }); // end each
+            
+            nameHTML += '</ul>';
+            nameHTML += '<button id="prev" class="btn role="button">« Previous</button>';
+            nameHTML += '<button id="next" class="btn role="button">Next »</button>';
+            $('#characters_cont').html("");
+            $('#movie_cont').html("");
+            $('#photos').html(nameHTML).modal();
+            
+            $('#next').click(function () {
+                $('#photos').html();
+                var nextText = data.next;
+                console.log(page);
+                if (page < 9) {
+                    page++;
+                    console.log(page);
+                    nameHTML = '<p>' + resource.toUpperCase() + ' Page: ' + page + '</p>';
+                    $.getJSON('http://swapi.co/api/' + resource + '/?page=' + page, function displayResource(data) {
+                        nameHTML += '<ul>';
+                        $.each(data.results, function (i, name) {
+                            nameHTML += '<li class="names">';
+                            nameHTML += '<h2>' + name.name  + '</h2>';
+                            nameHTML += '</li>';
+                        }); // end each
+
+                        nameHTML += '</ul>';
+                        nameHTML += '<button id="prev" class="btn role="button">« Previous</button>';
+                        nameHTML += '<button id="next" class="btn role="button">Next »</button>';
+                        $('#characters_cont').html("");
+                        $('#movie_cont').html("");
+                        $('#photos').html(nameHTML).modal();
+                    });
+                    console.log(nextText);
+                }//End IF
+            });//End $(#next).click()
+        }//End displayResource()
+        $.getJSON(swapiAPI, swapiOptionsR, displayResource);
+    });
+    
+        //Get film data
+    $('.the_films').click(function () {
+        $('#photos').html("");
+        $('#characters_cont').html("");
+        $.getJSON('http://swapi.co/api/films/', function (filmResponse) {
+            films = filmResponse.results;
+            filmsHTML = '<ul>';
+
+            //Loop thru film data
+            $.each(films, function (i, filmData) {
+                filmsHTML += '<li class="filmTitle">';
+                filmsHTML += filmData.title;
+                filmsHTML += '</li>';
+            });// end .each(filmData)
+            filmsHTML += '</ul>';
+            $('#movie_cont').html(filmsHTML);
+        });
+    });
+}); // end ready
---
+++ js/film_app.js
@@ -0,0 +1,56 @@
+$('.film_app').click(function() {
+    var page = 1;
+    $('#photos').html("");
+    $('#movie_cont').html("");
+    //Get film data
+    $.getJSON('http://swapi.co/api/films/', function(filmResponse) {
+        var films = filmResponse.results;
+        /*var filmsHTML = '<ul>';
+        
+        //Loop thru film data
+        $.each(films, function(i, filmData) {
+            filmsHTML += '<li class="filmTitle">';
+            filmsHTML += filmData.title;
+            filmsHTML += '</li>';
+        });// end .each(filmData)
+        filmsHTML += '</ul>';
+        $('#movie_cont').html(filmsHTML);*/
+        
+        //Get People data
+        $.getJSON('http://swapi.co/api/people/?page=' + page, function(peopleResponse){
+            var people = peopleResponse.results;
+            var peopleHTML = '<ul>';
+
+                //Loop thru people results
+                $.each(people, function(i, peopleData) {
+                    peopleHTML += '<li class="name">';
+                    peopleHTML += '<h2>' + peopleData.name + '</h2>';
+                    if(peopleData.films.length){
+                        peopleHTML += '<h3>Appeared in the following film(s):</h3>';
+                        peopleHTML += '<ul class="film_name">';
+                        
+                        //Loop thru films data
+                        $.each(peopleData.films, function(i, filmURL) {
+                            for (var c = 0; c < films.length; c++) {
+                                if (films[c].url === filmURL) {
+                                    
+                                    peopleHTML += '<li>';
+                                    peopleHTML += films[c].title;
+                                    
+                                    peopleHTML += '</li>';
+                                    break;   
+                                }//end if films match
+                            }
+                           
+                        });//end $.each(filmURL)
+                        peopleHTML += '</ul>';
+                    } //end if films have length
+                      
+                    peopleHTML += '</li>';
+                }); // end $.each(peopleData)
+                peopleHTML += '</ul>';
+                $('#characters_cont').html(peopleHTML);
+            
+        }); // end JSON(peopleResponse)
+    }); // end JSON(filmResponse)
+});    
